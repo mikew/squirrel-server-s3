40f99202895184f729daba140ee6ec49f352c038
Mike Wyatt <wyatt.mike@gmail.com>
2016-11-02 12:10:08 -0300

    implement getReleasesFile

--- src/getReleasesFile.js
+++ src/getReleasesFile.js
@@ -2,6 +2,20 @@ import config from './config'
 import { s3Instance } from './aws'
 
 export default function getReleasesFile () {
+  const params = {
+    Bucket: config.BUCKET,
+    Key: 'RELEASES',
+  }
+
   return new Promise((resolve, reject) => {
+    s3Instance.getObject(params, (err, data) => {
+      if (err) {
+        reject(err)
+
+        return
+      }
+
+      resolve(new Buffer(data.Body).toString())
+    })
   })
 }
