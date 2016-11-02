8c24e62e422946f2782473b0dc4e0a37beca0600
Mike Wyatt <wyatt.mike@gmail.com>
2016-11-02 12:08:11 -0300

    add isInstaller, isUpdate props to getUrlForVersion

--- spec/getUrlForVersion.spec.js
+++ spec/getUrlForVersion.spec.js
@@ -11,13 +11,32 @@ describe('getUrlForVersion', () => {
 
   it('works', () => {
     assert.strictEqual(
-      getUrlForVersion('1.0.0', 'darwin_x64'),
-      `https://example-app.s3-accelerated.amazonaws.com/1.0.0/Example%20App-v1.0.0-darwin_x64.zip`
+      `https://example-app.s3-accelerated.amazonaws.com/1.0.0/Example%20App-v1.0.0-darwin_x64.zip`,
+      getUrlForVersion('1.0.0', 'darwin_x64')
     )
 
     assert.strictEqual(
-      getUrlForVersion('1.0.0', 'linux_x86'),
-      `https://example-app.s3-accelerated.amazonaws.com/1.0.0/Example%20App-v1.0.0-linux_x86.zip`
+      `https://example-app.s3-accelerated.amazonaws.com/1.0.0/Example%20App-v1.0.0-linux_x86.zip`,
+      getUrlForVersion('1.0.0', 'linux_x86')
+    )
+  })
+
+  it('takes a `isInstaller` prop', () => {
+    assert.strictEqual(
+      `https://example-app.s3-accelerated.amazonaws.com/1.0.0/Example%20App-v1.0.0-darwin_x64.dmg`,
+      getUrlForVersion('1.0.0', 'darwin_x64', { isInstaller: true })
+    )
+
+    assert.strictEqual(
+      `https://example-app.s3-accelerated.amazonaws.com/1.0.0/Example%20App%20Setup-v1.0.0-win32_x64.exe`,
+      getUrlForVersion('1.0.0', 'win32_x64', { isInstaller: true })
+    )
+  })
+
+  it('takes a `isUpdate` prop', () => {
+    assert.strictEqual(
+      `https://example-app.s3-accelerated.amazonaws.com/1.0.0/example-app-v1.0.0-win32_x86-full.nupkg`,
+      getUrlForVersion('1.0.0', 'win32_x86', { isUpdate: true })
     )
   })
 })
--- src/getUrlForVersion.js
+++ src/getUrlForVersion.js
@@ -1,5 +1,27 @@
 import config from './config'
 
-export default function getUrlForVersion (version, platform) {
-  return `https://${config.BUCKET}.${config.S3_ENDPOINT}/${version}/${global.encodeURIComponent(config.APP_NAME)}-v${version}-${platform}.zip`
+export default function getUrlForVersion (version, platform, opts = {}) {
+  let namePart = config.APP_NAME
+  let ext = '.zip'
+
+  if (opts.isInstaller) {
+    if (platform.indexOf('win32') !== -1) {
+      namePart = `${namePart} Setup`
+      ext = '.exe'
+    }
+
+    if (platform.indexOf('darwin') !== -1) {
+      ext = '.dmg'
+    }
+  }
+
+  if (opts.isUpdate) {
+    if (platform.indexOf('win32') !== -1) {
+      namePart = namePart.toLowerCase().replace(/\s+/, '-')
+      ext = '-full.nupkg'
+    }
+  }
+
+  // eslint-disable-next-line max-len
+  return `https://${config.BUCKET}.${config.S3_ENDPOINT}/${version}/${global.encodeURIComponent(namePart)}-v${version}-${platform}${ext}`
 }
